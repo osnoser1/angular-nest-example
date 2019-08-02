b2d34b7fb25e6f07235b61a6983e1486c45aa3ea
Alfonso Andrés López Molina <osnoser_12@hotmail.com>
2019-08-01 22:08:15 -0500

    feat(api): agregadas validaciones

--- apps/api/src/users/dtos/create-user.dto.ts
+++ apps/api/src/users/dtos/create-user.dto.ts
@@ -1,6 +1,19 @@
-export interface CreateUserDto {
+import { IsNotEmpty, IsNumber, IsString, IsDateString } from 'class-validator';
+
+export class CreateUserDto {
+  @IsNotEmpty()
+  @IsNumber()
   document: number;
+
+  @IsNotEmpty()
+  @IsString()
   name: string;
+
+  @IsNotEmpty()
+  @IsString()
   lastName: string;
+
+  @IsNotEmpty()
+  @IsDateString()
   birthDate: string;
 }
